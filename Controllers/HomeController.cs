23fff4dff615c13440573276bd1184c6f59093e8
Damup1988 <damup1988@gmail.com>
2021-08-01 01:39:12 +0300

    Chapter 4 - Applying the async and await Keywords

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using LanguageFeatures.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +10,10 @@ namespace LanguageFeatures.Controllers
     public class HomeController : Controller
     {
         //public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
-            IProductSelection cart = new ShoppingCart(
-                new Product {Name = "Kayak", Price = 120M},
-                new Product {Name = "Lifejacket", Price = 50.50M},
-                new Product {Name = "Soccer ball", Price = 30.50M},
-                new Product {Name = "Corner flag", Price = 100M}
-            );
-            return View(cart.Names);
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] {$"Length: {length}"});
         }
     }
 }
